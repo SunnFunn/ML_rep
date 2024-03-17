a9208e2788bc1ae1eeea9937dbc582775420477c
Alexey <alext2370@outlook.com>
2024-03-17 16:29:24 +0300

    renew module added and other files

--- heat_project_RL/app/model_RL.py
+++ heat_project_RL/app/model_RL.py
@@ -1,6 +1,5 @@
 #общие библиотеки
 import numpy as np
-import random
 from app import bounds
 
 #фреймворк для работы с тензорами и нейросетями
@@ -81,5 +80,71 @@ class FCDP(nn.Module):
         x = self.out_activation_fc(x)
         return self.rescale_fn(x)
 
+#Модель Q функции
+class FCQV(nn.Module):
+    def __init__(self,
+                 input_dim,
+                 output_dim,
+                 hidden_dims=(32,32),
+                 activation_fc=F.relu):
+        super(FCQV, self).__init__()
+        self.activation_fc = activation_fc
+
+        self.input_layer = nn.Linear(input_dim, hidden_dims[0])
+        self.hidden_layers = nn.ModuleList()
+        for i in range(len(hidden_dims)-1):
+            in_dim = hidden_dims[i]
+            if i == 0:
+                in_dim += output_dim
+            hidden_layer = nn.Linear(in_dim, hidden_dims[i+1])
+            self.hidden_layers.append(hidden_layer)
+        self.output_layer = nn.Linear(hidden_dims[-1], 1)
+
+        device = "cpu"
+        if torch.cuda.is_available():
+            device = "cuda:0"
+        self.device = torch.device(device)
+        self.to(self.device)
+
+    def _format(self, state, action):
+        x, u = state, action
+        if not isinstance(x, torch.Tensor):
+            x = torch.tensor(x,
+                             device=self.device,
+                             dtype=torch.float32)
+            x = x.unsqueeze(0)
+        if not isinstance(u, torch.Tensor):
+            u = torch.tensor(u,
+                             device=self.device,
+                             dtype=torch.float32)
+            u = u.unsqueeze(0)
+        return x, u
+
+    def forward(self, state, action):
+        x, u = self._format(state, action)
+        x = self.activation_fc(self.input_layer(x))
+        for i, hidden_layer in enumerate(self.hidden_layers):
+            if i == 0:
+                x = torch.cat((x, u), dim=1)
+            x = self.activation_fc(hidden_layer(x))
+        return self.output_layer(x)
+
+    def load(self, experiences):
+        states, actions, rewards, new_states = experiences
+        states = torch.from_numpy(states).float().to(self.device)
+        actions = torch.from_numpy(actions).float().to(self.device)
+        rewards = torch.from_numpy(rewards).float().to(self.device)
+        new_states = torch.from_numpy(new_states).float().to(self.device)
+        return states, actions, rewards, new_states
+
+# custom weights initialization called on netG and netD
+def weights_init(m):
+    classname = m.__class__.__name__
+    if isinstance(m, nn.Linear):
+        nn.init.xavier_uniform_(m.weight)
+        m.bias.data.fill_(0.01)
+
 eval_strat = GreedyStrategy(bounds=bounds)
-model_RL = FCDP(7, bounds, hidden_dims=(128,256)).eval().to(DEVICE)
+p_model_RL = FCDP(8, bounds, hidden_dims=(128, 256, 256)).to(DEVICE)
+q_model_RL = FCQV(8, 1, hidden_dims=(128, 256, 256)).to(DEVICE)
+#q_model_RL.apply(weights_init)
--- heat_project_RL/app/parsing.py
+++ heat_project_RL/app/parsing.py
@@ -5,19 +5,19 @@ import urllib3
 import re
 from app import current_hour
 
-header = {'user-agent': 'Edg/95.0.1020.53'}
 urllib3.disable_warnings()
-url = 'https://rp5.ru/%D0%9F%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0_%D0%B2_%D0%95%D0%BA%D0%B0%D1%82%D0%B5%D1%80%D0%B8%D0%BD%D0%B1%D1%83%D1%80%D0%B3%D0%B5'
 
-response = req.get(url, headers=header, timeout=50, verify=False)
-page = BeautifulSoup(response.text,'html.parser')
+def get_temperatures_forecast(url):
+	response = req.get(url, headers = {'user-agent': 'Edg/95.0.1020.53'}, timeout=50, verify=False)
+	page = BeautifulSoup(response.text, 'html.parser')
 
-def get_temperatures_forecast():
 	#содержимое таблицы на странице прогноза погоды на 7 суток
 	table_content = page.find('table', {'id': 'forecastTable_1_3'})
 
-	# парсинг текущей температуры наружного воздуха
-	temperature = float(table_content.find('div', {'class': 't_0'}).text)
+	# парсинг текущей температуры наружного воздуха и прогноза температуры через 3 часа
+	temperatures_list = table_content.find_all('div', {'class': 't_0'})
+	temperature_current = float(temperatures_list[0].text)
+	temperature_next = float(temperatures_list[3].text)
 
 	#парсинг текущей скорости ветра
 	winds = table_content.find_all('div', {'class': 'wv_0'})
@@ -38,4 +38,4 @@ def get_temperatures_forecast():
 	cloudiness_info = table_content.find('div', {'class': 'cc_0'}).find('div')['onmouseover']
 	cloudiness = float(re.sub('[^0-9]', '', cloudiness_info)[:2])/100
 
-	return temperature, wind, humidity, cloudiness
+	return temperature_current, temperature_next, wind, humidity, cloudiness
